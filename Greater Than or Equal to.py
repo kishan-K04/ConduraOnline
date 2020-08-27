e10ae7fb1cc13013610f592b6ebdd5e842f01551
kishan-K04 <68174249+kishan-K04@users.noreply.github.com>
2020-08-27 16:37:07 +0530

    Add files via upload

---
+++ Greater Than or Equal to.py
@@ -0,0 +1,17 @@
+'''In Condura-online a web-platform where a person can learn aplitude 
+as well as programing by joining the websit or by creating an account there.
+
+In the programing hub they as to write the code where two iput parameter (integer)
+are given and tell what which is greater number in the form of 'Yes' or 'No'.
+
+in the below case if we give a=5 and b=2
+then the out will be 'Yes' as (a  variable is larger)'''  
+
+
+
+a=input()
+b=input()
+if a>=b:
+    print('Yes')
+else:
+    print('No')
