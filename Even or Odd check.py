37342fde6c0fbbb1802836db6f20961d6275b6e1
kishan-K04 <68174249+kishan-K04@users.noreply.github.com>
2020-08-27 16:49:43 +0530

    Add files via upload

---
+++ Even or Odd check.py
@@ -0,0 +1,10 @@
+'''In Condura-online a web-platform where a person can learn aplitude 
+as well as programing by joining the websit or by creating an account there.
+
+In the programing hub they were as to write the code, where to check wether 
+given number is even or odd '''
+a=int(input())
+if (a%2==0): #gives the reminder (% =>Modulus operator)
+    print("Even") 
+else:
+    print("Odd")
